f486372f2f31c891490c460e75c68724dd3c6926
sq-dev <sqmailer@gmail.com>
2023-03-22 22:25:10 +0500

    Feature: added CI\CD

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ echo "Deployment started ..."
 composer install --no-dev --no-interaction --prefer-dist
 php artisan optimize:clear
 php artisan optimize
-php artisan migrate
+php artisan migrate --force
 php artisan up
 
 echo "Deployment finished!"
