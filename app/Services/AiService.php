ca77a044de8b864cc0c737e14eab1da6ab6797d3
Asrorov Saidazam <sqmailer@gmail.com>
2024-02-03 02:31:25 +0500

    added Ai support to bot

---
+++ app/Services/AiService.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Services;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Support\Facades\Cache;
+
+class AiService
+{
+    public const API_URL = 'http://5.75.253.198:10000';
+
+    protected string $prompt = 'Представь что ты телеграм бот созданный для напоминания задачи, пользователя зовут %s, нужно ответить как клоун';
+
+    protected Client $client;
+
+    public function __construct()
+    {
+        $this->client = new Client([
+            'base_uri' => self::API_URL,
+            'http_errors' => false,
+            'verify' => false,
+            'headers' => [
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json'
+            ]
+        ]);
+    }
+
+    /**
+     * @throws GuzzleException
+     * @throws \JsonException
+     * @throws \Exception
+     */
+    public function complete(string $text, array $additional = [])
+    {
+        $name = $additional['name'] ?? 'Неизвестно';
+        $id = $additional['id'] ?? random_int(1, 1000);
+        $dialog = $this->getChatHistory($id);
+
+        $dialog[] = [
+            'role' => 'user',
+            'content' => $text
+        ];
+
+        $response = $this->client->post('/v1/chat/completions', [
+            'json' => [
+                'model' => 'gpt-3.5-turbo-16k-0613',
+                'stream' => false,
+                'messages' => [
+                    [
+                        'role' => 'system',
+                        'content' => sprintf($this->prompt, $name)
+                    ],
+                    ...$dialog
+                ]
+            ]
+        ]);
+
+        if (count($dialog) >= 50) {
+            unset($dialog[0]);
+        }
+
+        $answer = json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR)['choices']['0']['message']['content'];
+
+        $dialog[] = [
+            'role' => 'assistant',
+            'content' => $answer
+        ];
+
+       $this->setChatHistory($id, $dialog);
+
+        return $answer;
+    }
+
+    public function getChatHistory(int $id): array
+    {
+        return Cache::get('dialogs'.$id, []);
+    }
+
+    public function setChatHistory(int $id, array $history): void
+    {
+        Cache::put('dialogs'.$id, $history, 60 * 60 * 24 * 7);
+    }
+}
--- app/Telegram/Handlers/FallbackHandler.php
+++ app/Telegram/Handlers/FallbackHandler.php
@@ -2,12 +2,21 @@
 
 namespace App\Telegram\Handlers;
 
+use App\Services\AiService;
 use SergiX44\Nutgram\Nutgram;
+use Throwable;
 
 class FallbackHandler
 {
-    public function __invoke(Nutgram $bot): void
+    public function __invoke(Nutgram $bot, AiService $aiService): void
     {
-        $bot->sendMessage(__('text.unknown'));
+        try {
+            $aiService->complete($bot->message()->text, [
+                'name' => $bot->user()->first_name,
+                'id' => $bot->userId()
+            ]);
+        } catch (Throwable) {
+            $bot->sendMessage(__('text.unknown'));
+        }
     }
 }
